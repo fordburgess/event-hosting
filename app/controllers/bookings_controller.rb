d08e1fa7f1f68cf7d5f93837a0374ba11252539f
ford burgess <fburg6824@gmail.com>
2024-12-22 14:34:41 +0100

    finished pay confirmation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -30,11 +30,31 @@ class BookingsController < ApplicationController
 
   def payment_confirmation
     @booking = Booking.find(params[:id])
+
+    start_time = @booking.start_time
+    end_time = @booking.end_time
+
+    duration_in_seconds = end_time - start_time
+
+    @duration = nil
+    if duration_in_seconds >= 3600
+      @duration = "#{(duration_in_seconds / 3600).to_i} hours"
+    else
+      @duration = "#{(duration_in_seconds / 60).to_i} minutes"
+    end
+
+    duration_in_hours = duration_in_seconds / 3600.0
+    @total_payment = (@booking.venue.rate * duration_in_hours).round(2)
+
+    reviews = @booking.venue.reviews
+
+    @rating = (reviews.average(:rating).to_f).round(2)
+
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :guests, :start_time, :end_time)
   end
 end
