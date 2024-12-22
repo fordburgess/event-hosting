36694d4929944e6191b95a4162c1aed2c93775ad
ford burgess <fburg6824@gmail.com>
2024-12-23 00:35:16 +0100

    changed edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_22_140311) do
+ActiveRecord::Schema.define(version: 2024_12_22_232522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2024_12_22_140311) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "avatar"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
